a71032bedf19c81fbe76ca08f57cd684686907d1
Toogoolka <120179327+Toogoolka@users.noreply.github.com>
2023-01-28 19:25:25 +0300

    js

--- script/main.js
+++ script/main.js
@@ -1,11 +1,33 @@
-const button = document.querySelector("#delivery-btn")
-function delClick(){
-    alert("Click!")
-}
-button.addEventListener('click', delClick);
+// const button = document.querySelector("#delivery-btn")
+// function delClick(){
+//     alert("Click!")
+// }
+// button.addEventListener('click', delClick);
 
-// const lang = prompt("Введите название языка программирования:");
-// // const collection = {
-// //     [lang]: 'Лучший язык программирования!', // имя свойства будет взято из переменной lang
-// // };
-// // alert(collection[lang]);
+
+const btnPlus = document.querySelector("#plus__btn1");
+const btnSub = document.querySelector("#sub__btn1");
+const navDrinks = document.querySelector("#drinks");
+const navDesserts = document.querySelector("#desserts");
+const navFood = document.querySelector("#food")
+let inpField = document.querySelector("#capuccino");
+function plus(){
+    if(inpField.value < 999){
+        inpField.value ++;
+    }else {
+        inpField.value = 999;
+        alert("Превышено максимально допустимое количество!")
+    }
+    console.log('Количество увеличилось на 1');
+}
+function sub(){
+    if(inpField.value > 0){
+        inpField.value --;
+    }else {
+        inpField.value = 0;
+    }
+
+    console.log("Количество уменьшилось на 1")
+}
+btnPlus.addEventListener('click', plus);
+btnSub.addEventListener('click', sub);
